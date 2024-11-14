30341efb6942d955e49aa773e210e9c742b1e947
papa145 <papa1945@gmail.com>
2024-11-14 23:44:24 +0900

    feat: Create landing page background with Three.js and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Scene from '@/components/Scene'
+'use client'
+import Scene from '@/components/ 3dObject/Scene'
+import PokemonTitle from '@/components/ui/pokemonTitle';
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
 
 export default function Home() {
   return (
-    <main>
-      <Scene />
-    </main>
+      <main className="flex flex-col h-screen w-full items-center justify-center ">
+         <PokemonTitle/>
+          <Scene/>
+      </main>
   )
 }
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="fixed top-0 w-full">
+      <nav className="container mx-auto px-4 py-4">
+        {/* 네비게이션 내용 */}
+      </nav>
+    </header>
+  )
+}
