06a70b7c1ebbe92d66b0c54a0d29390c3cfdb4a7
papa145 <papa1945@gmail.com>
2024-11-16 00:42:57 +0900

    fix lint for "`"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <main className="flex flex-col h-screen w-full items-center justify-center ">
          <PokemonTitle/>
           <Scene/>
-          <Button variant={"outline"} className={`${bangers.className} bg-gradient-to-b from-amber-300 to-amber-400 px-10 py-5 text-xl rounded-lg border-b-4 border-amber-500 active:border-b-0 active:translate-y-1 transform transition-all`}>Let's Go</Button>
+          <Button variant={"outline"} className={`${bangers.className} bg-gradient-to-b from-amber-300 to-amber-400 px-10 py-5 text-xl rounded-lg border-b-4 border-amber-500 active:border-b-0 active:translate-y-1 transform transition-all`}>Let&apos;s Go</Button>
       </main>
   )
 }
