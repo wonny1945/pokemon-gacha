89504afe8f2753296e77255e1d8e8a4c6b3f2272
papa145 <papa1945@gmail.com>
2024-11-16 23:33:15 +0900

    feat: Implement header menu and let's go button animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 'use client'
 import Scene from '@/components/ 3dObject/Scene'
 import PokemonTitle from '@/components/ui/pokemonTitle';
-import {Button} from "@/components/ui/button";
-import { Bangers } from 'next/font/google'
+import StartButton from "@/components/ui/startButton";
 
-
-const bangers = Bangers({
- subsets: ['latin'],
- weight: '400'
-})
 
 
 export default function Home() {
@@ -16,7 +10,7 @@ export default function Home() {
       <main className="flex flex-col h-screen w-full items-center justify-center ">
          <PokemonTitle/>
           <Scene/>
-          <Button variant={"outline"} className={`${bangers.className} bg-gradient-to-b from-amber-300 to-amber-400 px-10 py-5 text-xl rounded-lg border-b-4 border-amber-500 active:border-b-0 active:translate-y-1 transform transition-all`}>Let&apos;s Go</Button>
+          <StartButton/>
       </main>
   )
 }
---
+++ src/components/ui/startButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+import {Button} from "@/components/ui/button";
+import {Bangers} from 'next/font/google'
+
+const bangers = Bangers({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+
+export default function StartButton() {
+    return (
+        <div>
+            <Button
+                variant="outline"
+                className={`
+        ${bangers.className}
+        bg-gradient-to-b from-amber-300 to-amber-400 
+        px-10 py-5 text-2xl rounded-lg 
+        border-b-4 border-amber-500 
+        transition-all duration-1
+        animate-bounce 
+      `}>
+                Let&apos;s Go
+            </Button>
+        </div>
+    )
+}
